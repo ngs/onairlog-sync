421b479298c592181fb9e8a0817d220b65588983
Atsushi Nagase <a@ngs.io>
2020-06-19 16:29:03 +0900

    Slack Notify

--- go.mod
+++ go.mod
@@ -8,10 +8,15 @@ require (
 	github.com/GoogleCloudPlatform/functions-framework-go v1.0.1
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/ashwanthkumar/slack-go-webhook v0.0.0-20200209025033-430dd4e66960
+	github.com/elazarl/goproxy v0.0.0-20200426045556-49ad98f6dac1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/jinzhu/gorm v1.9.14
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/parnurzeal/gorequest v0.2.16 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
+	moul.io/http2curl v1.0.0 // indirect
 )
--- notify.go
+++ notify.go
@@ -3,17 +3,42 @@ package onairlogsync
 import (
 	"context"
 	"encoding/json"
-	"log"
+	"fmt"
+
+	"github.com/ashwanthkumar/slack-go-webhook"
 )
 
 func Notify(ctx context.Context, m PubSubMessage) error {
 	var songs []Song
+	webhookUrl := mustGetenv("SLACK_WEBHOOK_URL")
 	app := NewApp(ctx)
 	err := json.Unmarshal(m.Data, &songs)
 	if err != nil {
 		app.LogError(err)
 		return err
 	}
-	log.Println(songs)
+
+	for _, song := range songs {
+		timeStr := (*song.Time).Format("2006/01/02 15:04")
+		fallback := fmt.Sprintf("%s %s / %s", timeStr, song.Title, song.Artist)
+		ts := (*song.Time).Unix()
+
+		attachment1 := slack.Attachment{}
+		attachment1.Fallback = &fallback
+		attachment1.AuthorName = &song.Artist
+		attachment1.Title = &song.Title
+		attachment1.Timestamp = &ts
+		payload := slack.Payload{
+			Attachments: []slack.Attachment{attachment1},
+		}
+		errors := slack.Send(webhookUrl, "", payload)
+		if len(errors) > 0 {
+			for _, err := range errors {
+				app.LogError(err)
+			}
+			return errors[0]
+		}
+	}
+
 	return nil
 }
